66dec55a0cb0f9072ee2e4b74fcae1e2f53682f3
tonyducks <tonyducks@gmail.com>
2015-08-15 10:13:27 -0500

    actualización del proyecto usando JUCE v.3.2

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -37,6 +37,11 @@
 #define JUCE_MODULE_AVAILABLE_juce_gui_extra             1
 
 //==============================================================================
+#ifndef    JUCE_STANDALONE_APPLICATION
+ #define   JUCE_STANDALONE_APPLICATION 1
+#endif
+
+//==============================================================================
 // dRowAudio flags:
 
 #ifndef    DROWAUDIO_USE_FFTREAL
@@ -60,6 +65,10 @@
 
 #ifndef    JUCE_WASAPI
  //#define JUCE_WASAPI
+#endif
+
+#ifndef    JUCE_WASAPI_EXCLUSIVE
+ //#define JUCE_WASAPI_EXCLUSIVE
 #endif
 
 #ifndef    JUCE_DIRECTSOUND
@@ -145,6 +154,10 @@
 
 #ifndef    JUCE_INCLUDE_ZLIB_CODE
  //#define JUCE_INCLUDE_ZLIB_CODE
+#endif
+
+#ifndef    JUCE_USE_CURL
+ //#define JUCE_USE_CURL
 #endif
 
 //==============================================================================
